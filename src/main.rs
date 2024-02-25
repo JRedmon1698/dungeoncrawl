ef18138b4726f20a05ff8c3b6c7d1eb967af36ab
Joseph Redmon <josephredmon@Josephs-MacBook-Pro.local>
2024-02-25 14:55:04 -0800

    add components mod, Render struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod camera;
+mod components;
 mod map;
 mod map_builder;
 
@@ -10,6 +11,7 @@ mod prelude {
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::camera::*;
+    pub use crate::components::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
 }
