9d7a46837c342deae9c83027c6ab556552d1d265
Joseph Redmon <josephredmon@Josephs-MacBook-Pro.local>
2024-02-27 19:08:29 -0800

    working crawler now, but very small

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ mod systems;
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub use legion::*;
-    pub const SCREEN_WIDTH: i32 = 80;
-    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const SCREEN_WIDTH: i32 = 200;
+    pub const SCREEN_HEIGHT: i32 = 150;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::camera::*;
