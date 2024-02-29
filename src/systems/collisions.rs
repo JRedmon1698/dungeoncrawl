e577c9172e5bb42b94ae2487b0e046da8d3ad424
Joe Redmon <joe.redmon@emsibg.com>
2024-02-29 12:59:53 -0800

    wip collisions func in systems

---
+++ src/systems/collisions.rs
@@ -0,0 +1,9 @@
+use legion::world::SubWorld;
+
+use crate::prelude::*;
+
+#[system]
+#[read_component(Point)]
+#[read_component(Player)]
+#[read_component(Enemy)]
+pub fn collisions(ecs: &mut SubWorld, commands: &mut CommandBuffer) {}
