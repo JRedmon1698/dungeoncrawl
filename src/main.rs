2d2dd62d40f7a428482d730b0b861b6475cab366
Joseph Redmon <josephredmon@Josephs-MacBook-Pro.local>
2024-03-30 19:07:53 -0700

    less brackets in match

--- src/main.rs
+++ src/main.rs
@@ -162,20 +162,14 @@ impl GameState for State {
             TurnState::AwaitingInput => self
                 .input_systems
                 .execute(&mut self.ecs, &mut self.resources),
-            TurnState::PlayerTurn => {
-                self.player_systems
-                    .execute(&mut self.ecs, &mut self.resources);
-            }
-            TurnState::MonsterTurn => {
-                self.monster_systems
-                    .execute(&mut self.ecs, &mut self.resources);
-            }
-            TurnState::GameOver => {
-                self.game_over(ctx);
-            }
-            TurnState::Victory => {
-                self.victory(ctx);
-            }
+            TurnState::PlayerTurn => self
+                .player_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::MonsterTurn => self
+                .monster_systems
+                .execute(&mut self.ecs, &mut self.resources),
+            TurnState::GameOver => self.game_over(ctx),
+            TurnState::Victory => self.victory(ctx),
         }
 
         render_draw_buffer(ctx).expect("Render error");
