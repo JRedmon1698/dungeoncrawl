77d5ea2bdb643012bb93f4bc03440eec18035a5e
Joe Redmon <joe.redmon@emsibg.com>
2024-04-11 13:04:28 -0700

    oops, did not finish making monsters unseen on non-visible tiles, fixed

--- src/systems/entity_render.rs
+++ src/systems/entity_render.rs
@@ -8,7 +8,6 @@ use crate::prelude::*;
 pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
     let mut renderables = <(&Point, &Render)>::query();
     let mut fov = <&FieldOfView>::query().filter(component::<Player>());
-    let player_fov = fov.iter(ecs).nth(0).unwrap();
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(1);
 
@@ -17,11 +16,13 @@ pub fn entity_render(ecs: &SubWorld, #[resource] camera: &Camera) {
         .for_each(|(pos, render)| {
             let offset = Point::new(camera.left_x, camera.top_y);
 
+            let player_fov = fov.iter(ecs).nth(0).unwrap();
             renderables
                 .iter(ecs)
                 .filter(|(pos, _)| player_fov.visible_tiles.contains(&pos))
-                .for_each(|(pos, _)| {});
-            draw_batch.set(*pos - offset, render.color, render.glyph);
+                .for_each(|(pos, render)| {
+                    draw_batch.set(*pos - offset, render.color, render.glyph);
+                });
         });
     draw_batch.submit(5000).expect("Batch error: entity_render");
 }
