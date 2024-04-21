94e1e6a28e5f327b927195b4751bb1f7c10d8a35
Joseph Redmon <josephredmon@Josephs-MacBook-Pro.local>
2024-04-21 10:32:58 -0700

    use rooms arch instead of empty arch in new func for map builder

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -21,7 +21,7 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = EmptyArchitect {};
+        let mut architect = RoomsArchitect {};
         architect.new(rng)
     }
 
