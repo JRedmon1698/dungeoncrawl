2d0c99096a79646ec98beb71431352570d284b99
Joe Redmon <joe.redmon@emsibg.com>
2024-04-27 19:51:55 -0400

    start drunkards walk alg

---
+++ src/map_builder/drunkard.rs
@@ -0,0 +1,18 @@
+use super::MapArchitect;
+use crate::prelude::*;
+
+pub struct DrunkardsWalkArchitect {}
+
+impl MapArchitect for DrunkardsWalkArchitect {
+    fn new(&mut self, rng: RandomNumberGenerator) -> MapBuilder {
+        let mut mb = MapBuilder {
+            map: Map::new(),
+            rooms: Vec::new(),
+            monster_spawns: Vec::new(),
+            player_start: Point::zero(),
+            amulet_start: Point::zero(),
+        };
+
+        mb
+    }
+}
